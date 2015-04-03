0be95dc68612a811b537a580f1ded482e42afeef
danielzmud1 <danielzmud1@gmail.com>
2015-04-03 18:49:15 +0200

    zmiana

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("zmiana");
-            Console.WriteLine("kolejna kolejna konfliktowa linia");
+            Console.WriteLine("konfliktowa linia");
             Console.ReadKey();
         }
     }
